cbddc7038b26d4840a4e1d9de45380c0b4a9e33c
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2020-11-22 14:46:26 -0800

    update Landing page and login page QuickSave
    landing page works to login. but not login to site yet

---
+++ MVCPrbSol/Views/Home/LandingPage.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCPrbSol.Views.Home
+{
+    public class PSLandingPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
